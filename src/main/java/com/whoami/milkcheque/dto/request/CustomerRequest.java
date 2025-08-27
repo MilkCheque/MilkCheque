a6ea4e9e1d7e2087257b871970b55d6c1c16da35
Rola Khaled <rolatotta2004@gmail.com>
2025-08-27 13:10:31 +0300

    add CustomerRequest

---
+++ src/main/java/com/whoami/milkcheque/dto/request/CustomerRequest.java
@@ -0,0 +1,10 @@
+package com.whoami.milkcheque.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CustomerRequest {
+    private String customerName;
+    private String phoneNumber;
+    private String tableId;
+}
