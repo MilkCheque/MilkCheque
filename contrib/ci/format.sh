f88f26349cef12f6340189c3be6d21293825f680
Hamed Elgizery <hamedashraf2004@gmail.com>
2025-08-31 22:15:02 +0300

    Add google-java-format pre-commit

---
+++ contrib/ci/format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+jar_version="1.28.0"
+jar_dir="$HOME/.local/share/java"
+jar_file="$jar_dir/google-java-format-$jar_version-all-deps.jar"
+java_cmd="java"
+
+# download jar file if missing
+if [ ! -f "$jar_file" ]; then
+    mkdir -p "$jar_dir"
+    curl -o "$jar_file" "https://github.com/google/google-java-format/releases/download/v${jar_version}/google-java-format-${jar_version}-all-deps.jar"
+fi
+
+# execute formatter
+$java_cmd -jar "$jar_file" $@
