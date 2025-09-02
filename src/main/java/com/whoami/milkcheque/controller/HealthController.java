532b2afa3058d2f8e511f3ea145e4b9e11c4296e
yousseffyasser <youssefyasserammar@gmail.com>
2025-09-02 20:16:35 +0300

    Update HealthController.java

--- src/main/java/com/whoami/milkcheque/controller/HealthController.java
+++ src/main/java/com/whoami/milkcheque/controller/HealthController.java
@@ -1,12 +1,19 @@
 package com.whoami.milkcheque.controller;
 
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/test")
 public class HealthController {
   
     @GetMapping("/health")
-    public String healthChecker(){
-       return "All good!";
+    public String healthChecker() {
+        return "All Good!";
+    }
+
+    @GetMapping("/health2")
+    public String healthChecker2() {
+        return "Still good!";
     }
 }
